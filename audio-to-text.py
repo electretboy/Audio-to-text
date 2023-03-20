b21998d31da8ca41fa06b9a3f99db7c9c1fb2f9c
AngryRacoon <70721854+electretboy@users.noreply.github.com>
2023-03-20 14:31:51 +0100

    Update audio-to-text.py

--- audio-to-text.py
+++ audio-to-text.py
@@ -1,4 +1,18 @@
 import whisper
+import tkinter as tk
+from tkinter import filedialog
+
+def browse_file():
+    file_path = filedialog.askopenfilename()
+    print(file_path)
+
+root = tk.Tk()
+root.withdraw()
+
+browse_button = tk.Button(text="Browse", command=browse_file)
+browse_button.pack()
+
+root.mainloop()
 
 model = whisper.load_model("tiny")
 result = model.transcribe("120.mp3")
